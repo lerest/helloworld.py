08a2fd83a02715caec15a0eb68e177b0d85a3b22
lerest <39830681+lerest@users.noreply.github.com>
2019-05-05 12:43:15 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,22 +1,10 @@
 #calculator V1
 
-from colorama import init
-from colorama import Fore, Back, Style
 
-init()
+what = input( "Що будемо робити? (+, - чи * ,/ ): ")
 
-print ( Fore.BLACK )
-
-print ( Back.GREEN )
-
-what = input( "Что будем делать? (+, - or * ,/ ): ")
-
-print ( Back.CYAN )
-
-a = float( input("Введите первое число: "))
-b = float( input("Ведите второе число: "))
-
-print ( Back.RED )
+a = float( input("Введіть перше число: "))
+b = float( input("Введіть друге число: "))
 
 if what == "+":
 	c = a + b
@@ -32,6 +20,6 @@ elif what == "/":
 	print ("Результат: " + str(c))
 
 else:
-	print("Выбранна неправильная операцыя")
+	print("Вибрана не вірна операція")
 
 input()
